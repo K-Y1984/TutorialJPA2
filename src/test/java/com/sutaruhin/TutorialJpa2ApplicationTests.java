b21f69c919ddde2898db5612f9ee0afbd8b89928
user <user@localhost>
2023-08-06 10:28:18 +0800

    TutorialJPAChapter 6

---
+++ src/test/java/com/sutaruhin/TutorialJpa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sutaruhin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialJpa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
